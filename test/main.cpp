fb42ac3f8589cd3f09cd6466f9aef71935ec2f5c
Situn <ksaswat007@gmail.com>
2022-03-10 16:53:02 +0000

    Added multiple test case support per test

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,13 @@
+#include<iostream>
+
+using namespace std;
 
 int main(){
+    int testCaseCount;
+    cin >> testCaseCount;
+    string baseType, subType;
+    while(testCaseCount--){
+        cin >> baseType >> subType;
+    }
     return 0;
 }
