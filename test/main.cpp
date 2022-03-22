da71f3609d2d7954c4ebef3de4bb19668e61ea9a
Situn <ksaswat007@gmail.com>
2022-03-22 11:02:44 +0000

    Added sort test case handling

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 
+#include "sort.hpp"
+
 using namespace std;
+
+void performSortOperation(string subType){
+    Sort *sortClass = Sort::getSortingClass(subType);
+    if(sortClass != nullptr){
+        int n, element;
+        cin >> n;
+        for(register int i = 0; i < n; ++i){
+            cin >> element;
+            sortClass->insert(element);
+        }
+        sortClass->sort();
+        sortClass->print();
+    }
+    else{
+        cout << "Subtype " << subType << " not found!!" << endl;
+    }
+}
 
 int main(){
     int testCaseCount;
@@ -8,6 +27,9 @@ int main(){
     string baseType, subType;
     while(testCaseCount--){
         cin >> baseType >> subType;
+        if(baseType == "sort"){
+            performSortOperation(subType);
+        }
     }
     return 0;
 }
