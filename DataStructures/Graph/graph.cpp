438679a8091f2c09590f5bce73ad887c458ffbe3
Situn <ksaswat007@gmail.com>
2022-04-10 15:37:24 +0000

    Added totalNodes member and Graph constructor

---
+++ DataStructures/Graph/graph.cpp
@@ -0,0 +1,5 @@
+#include"graph.hpp"
+
+Graph::Graph(int n){
+    totalNodes = n;
+}
--- DataStructures/Graph/graph.hpp
+++ DataStructures/Graph/graph.hpp
@@ -2,7 +2,10 @@
 #define __GRAPH
 
 class Graph{
+protected:
+    int totalNodes;
 public:
+    Graph(int n);
     virtual void addEdge(int source, int sink) = 0;
     virtual void removeEdge(int source, int sink) = 0;
 };
