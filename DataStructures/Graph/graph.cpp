269a5c094c1484d9087e63ec3d474feb198f4b1d
Situn <ksaswat007@gmail.com>
2022-04-10 18:02:54 +0000

    Added static method Graph::getGraphObject()

--- DataStructures/Graph/graph.cpp
+++ DataStructures/Graph/graph.cpp
@@ -1,4 +1,17 @@
 #include"graph.hpp"
+#include"adjacencyMatrix.hpp"
+#include"adjacencyList.hpp"
+
+Graph* Graph::getGraphObject(GraphTypes type, int totalNodes){
+    switch(type)
+    {
+        case ADJACENCY_MATRIX:
+            return new AdjacencyMatrix(totalNodes);
+        case ADJACENCY_LIST:
+            return new AdjacencyList(totalNodes);
+    }
+    return nullptr;
+}
 
 Graph::Graph(int n){
     totalNodes = n;
--- DataStructures/Graph/graph.hpp
+++ DataStructures/Graph/graph.hpp
@@ -2,9 +2,15 @@
 #define __GRAPH
 
 class Graph{
+private:
+    enum GraphTypes{
+        ADJACENCY_MATRIX,
+        ADJACENCY_LIST
+    };
 protected:
     int totalNodes;
 public:
+    static Graph* getGraphObject(GraphTypes type, int totalNodes);
     Graph(int n);
     virtual void addEdge(int source, int sink) = 0;
     virtual void removeEdge(int source, int sink) = 0;
